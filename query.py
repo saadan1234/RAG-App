c29620cbd1ba283ca9abc0383a77a25413bd6330
saadan1234 <msaadan.bese21seecs@seecs.edu.pk>
2024-07-07 07:09:36 -0700

    UI

--- query.py
+++ query.py
@@ -34,7 +34,7 @@ def query_rag(query_text: str, model: str):
     # Search the DB.
     results = db.similarity_search_with_score(query_text, k=3)
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc in results])
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     # print(prompt)
@@ -44,7 +44,7 @@ def query_rag(query_text: str, model: str):
         model = Ollama(model="llama3")
     response_text = model.invoke(prompt)
 
-    sources = [doc.metadata.get("id", None) for doc in results]
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
     return formatted_response
